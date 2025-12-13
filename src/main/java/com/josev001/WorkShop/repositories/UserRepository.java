cd168b6ba944de2ae6d0cd443545e1ee6c2b2539
JoseV-001 <Leaphaz@gmail.com>
2025-12-12 22:22:55 -0300

    Service layer, component registration

--- src/main/java/com/josev001/WorkShop/repositories/UserRepository.java
+++ src/main/java/com/josev001/WorkShop/repositories/UserRepository.java
@@ -3,5 +3,6 @@ package com.josev001.WorkShop.repositories;
 import com.josev001.WorkShop.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
---
+++ src/main/java/com/josev001/WorkShop/services/UserService.java
@@ -0,0 +1,27 @@
+package com.josev001.WorkShop.services;
+
+import com.josev001.WorkShop.entities.User;
+import com.josev001.WorkShop.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User finById(Long id){
+       Optional<User> obj = repository.findById(id);
+       return obj.get();
+    }
+
+}
