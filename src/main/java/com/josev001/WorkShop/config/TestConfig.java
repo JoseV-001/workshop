b7a647ae57cac48f70f386afde419f9ea9283f0a
JoseV-001 <Leaphaz@gmail.com>
2025-12-15 22:10:55 -0300

    OrderStatus enum

--- src/main/java/com/josev001/WorkShop/config/TestConfig.java
+++ src/main/java/com/josev001/WorkShop/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.josev001.WorkShop.config;
 
 import com.josev001.WorkShop.entities.Order;
 import com.josev001.WorkShop.entities.User;
+import com.josev001.WorkShop.entities.enums.OrderStatus;
 import com.josev001.WorkShop.repositories.OrderRepository;
 import com.josev001.WorkShop.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Jose", "jose@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Cristovao", "cristovao@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
